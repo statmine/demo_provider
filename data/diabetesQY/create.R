ed6bf01c05b71d19ce990929f66c2b15f0e391dc
Edwin de Jonge <edwindjonge@gmail.com>
2016-09-21 08:01:12 +0200

    added a new table

---
+++ data/diabetesQY/create.R
@@ -0,0 +1,17 @@
+library(dplyr)
+
+data <- read.csv("../diabetes/data.csv")
+
+data <- 
+  data %>% 
+  rename(periode = jaar) %>% 
+#  filter(leeftijd == "Totaal", geslacht=="Totaal") %>% 
+  select(periode, leeftijd, geslacht, suiker)
+
+
+data_q <- lapply(1:4, function(quarter){
+  periode <- paste0(data$periode, "Q", quarter)
+  data$suiker <- data$suiker + round(rnorm(n=nrow(data), sd=0.2), 0.1)
+  data
+})
+
