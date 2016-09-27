340ba6fd91593a525c35da59e1617a1b629272d5
Edwin de Jonge <edwindjonge@gmail.com>
2016-09-27 15:56:08 +0200

    making a Quarter data set

--- data/diabetesQY/create.R
+++ data/diabetesQY/create.R
@@ -5,13 +5,14 @@ data <- read.csv("../diabetes/data.csv")
 data <- 
   data %>% 
   rename(periode = jaar) %>% 
-#  filter(leeftijd == "Totaal", geslacht=="Totaal") %>% 
+#  filter(leeftijd == "Totaal", geslacht=="Totaal") %>%
+  mutate(periode = as.character(periode)) %>% 
   select(periode, leeftijd, geslacht, suiker)
 
 
 data_q <- lapply(1:4, function(quarter){
-  periode <- paste0(data$periode, "Q", quarter)
-  data$suiker <- data$suiker + round(rnorm(n=nrow(data), sd=0.2), 0.1)
+  data$periode <- paste0(data$periode, "Q", quarter)
+  data$suiker <- data$suiker + round(rnorm(n=nrow(data), sd=0.2), 1)
   data
 })
 
